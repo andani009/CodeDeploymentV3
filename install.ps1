5f4af4f94282d0508a94084fec510bbfa4e93b56
andani009 <andanayya.hiremath@sakhatech.com>
2019-12-02 16:39:55 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,2 +1,1 @@
-appcmd add site /name:example1.com /id:123 /physicalPath:C:\inetpub\wwwroot\src\HelloWorldWebCore\build_output /bindings:http/*:8078:localhost
 New-WebApplication -Site "Default Web Site" -Name MusicWorld -PhysicalPath C:\inetpub\wwwroot\src\HelloWorldWebCore\build_output -Force
