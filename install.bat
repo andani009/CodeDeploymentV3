14ded11d94652798a9b877a92b5fc3498eba6356
andani009 <andanayya.hiremath@sakhatech.com>
2019-12-02 12:55:33 +0530

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,1 @@
-
-cmd.exe /c C:\Users\Administrator\Desktop\script.bat
-
-
+appcmd add site /name:example.com /id:123 /physicalPath:C:\inetpub\wwwroot\src\HelloWorldWebCore\build_output /bindings:http/*:8088:localhost
