25901d882f21afe43137021029f3c48fcfb8e6dd
andani009 <andanayya.hiremath@sakhatech.com>
2019-12-02 10:53:43 +0530

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,14 +1,2 @@
-cd C:\Program Files\dotnet\sdk
-
-dotnet --version
-
-dir
-
-aws2 --version
-
-
 cd C:\inetpub\wwwroot\src\HelloWorldWebCore\build_output
-
-dotnet --version
-
 dotnet HelloWorldWebCore.dll
