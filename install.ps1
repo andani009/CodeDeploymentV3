820c6c7a5ae1fe805b1b502116726e6dceb9b18a
andani009 <andanayya.hiremath@sakhatech.com>
2019-12-02 17:10:04 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,2 +1,2 @@
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe
 New-WebApplication -Site "Default Web Site" -Name MusicWorld -PhysicalPath C:\inetpub\wwwroot\src\HelloWorldWebCore\build_output -Force
